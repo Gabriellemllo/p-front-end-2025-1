9146c44955f5d1aa103ea4ea9fa8952be600c918
Gabi Melo <gabriellesilvamelo622@gmail.com>
2025-02-25 00:10:18 +0000

    aula2

--- ex1/app/page.js
+++ ex1/app/page.js
@@ -1,16 +1,22 @@
+'use client'
 import Image from "next/image";
+import { useState } from "react";
 function Texto1(){
   return(
     <p>Um parágrafo de texto!</p>
   )
 }
 export default function Home() {
-  const aula =2;
+  const[hide,sethide] = useState(false);
+  const aula = 2;
   return (
     //<> ,</> é um tipo de div lembre-se 
    <div>
     <h1><strong>Olá Turma!!!</strong></h1>
     <p>Essa é a nossa {aula}ª aula de React!</p>
+    <hr/>
+    <button onClick={() => sethide(!hide)}>{hide ? "Show" : "Hide"}</button>
+    <hr/>
     <Texto1/>
     <Texto1/>
    </div>
